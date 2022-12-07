56462460882357af2d92935e593a9219cb8e34de
armycrih <armycrih@gmail.com>
2022-12-06 21:13:46 -0300

    update authentication

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ from users import views
 
 
 urlpatterns = [
-    path('list/', views.ListUserView.as_view()),
+    path('create/', views.CreateUserView.as_view()),
+    path('token/', views.CreateTokenView.as_view()),
+    path('user/', views.RetreiveUpdateUserView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,19 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
 
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, AuthTokenSerializer
 from .models import User
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     
-
-class ListUserView(generics.ListAPIView):
+class RetreiveUpdateUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    queryset = User.objects.all()
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     
+    def get_object(self):
+        return self.request.user
+    
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
